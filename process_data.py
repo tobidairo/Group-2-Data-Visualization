93a06f9b11f00c9f02a4434de365e3ca5855aa35
tobidairo <tobidairo@outlook.com>
2024-09-10 10:21:43 +0100

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -1,5 +1,11 @@
+import os
 import pandas as pd
 from mappings import state_mapping, income_bracket_midpoints, age_range_midpoints, dtypes
+from download_data import download_data
+
+if not os.path.exists('data.csv'):
+    print("Data file not found, downloading...")
+    download_data('1ZdsrtNY3H7Oh_ojb3vootMMyV84Kw002')
 
 def read_data(dtypes):
     df = pd.read_csv('data.csv', header=0, dtype=dtypes)
